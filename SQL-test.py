4dd1d4aed22968687499afdc28712590f1089440
moralesja <aalberto_morales@hotmail.com>
2022-07-01 18:26:57 -0500

    functional update

--- SQL-test.py
+++ SQL-test.py
@@ -3,13 +3,6 @@ from sqlalchemy.orm import sessionmaker
 import pyodbc
 import pandas as pd
 
-#engine = create_engine("dialect[SQL Server]://scadamex:scadamex@SAL-W12E-SQL//MSSQLMEX/scadadata")
-
-#"dialect[+driver]://+ dsn_uid + ':' + dsn_pwd + '@'+dsn_hostname+':'+dsn_port+'/' + dsn_database"
-
-#"dialect[SQL Server]://scadamex:scadamex@SAL-W12E-SQL\MSSQLMEX\scadadata"
-
-#engine = create_engine(r'mssql+pyodbc://scadamex:scadamex@SAL-W12E-SQL\MSSQLMEX\scadadata')
 
 engine = create_engine('mssql+pyodbc://scadamex:scadamex@SAL-W12E-SQL\MSSQLMEX/scadadata?driver=SQL+Server+Native+Client+11.0', echo=True)
 
@@ -24,7 +17,7 @@ print(df)
 
 mydict = {562: "Erik Gaytan",
 		568: "Jorge Perez",
-		986: "Alejandro Lopez",
+		986: "Alejandro Lopez", 
 		1471: "Omar Hernandez",
 		1476: "Pablo Garcia",
 		1881: "Eduardo Rodriguez",
@@ -45,4 +38,4 @@ nueva_info = pd.DataFrame(mydict.items(), columns=['Empl_Number', 'Empl_Name'])
 
 print(nueva_info)
 
-nueva_info.to_sql('Empleados_Mtto', con=engine, if_exists='replace')
+#nueva_info.to_sql('Empleados_Mtto', con=engine, if_exists='append')
---
+++ load_mtto.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pandas as pd
+import sys
+import os
+
+engine = create_engine('mssql+pyodbc://scadamex:scadamex@SAL-W12E-SQL\MSSQLMEX/scadadata?driver=SQL+Server+Native+Client+11.0', echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+mtto_df = pd.read_csv(resource_path(r"resources/mtto_data.csv"),header=0,usecols=['NUM','LINEA','MAQUINA','COMPONENTE','CONDICION_FALLA','ACCION_INMEDIATA','MAQUINA_1','CAUSA','Minutos_TM','FECHA','SEMANA','RESPONSABLE','TURNO','Aplica'])
+
+
+#nueva_info = pd.DataFrame(mtto_df, columns=['NUM','LINEA','MAQUINA','COMPONENTE','CONDICION_FALLA','ACCION_INMEDIATA','MAQUINA_1','CAUSA','Minutos_TM','FECHA','SEMANA','RESPONSABLE','TURNO','Aplica'])
+
+print(mtto_df)
+
+mtto_df.to_sql('Test2_MTTO_History', con=engine, if_exists='append')
